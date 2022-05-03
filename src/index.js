e5cac7a925d432bec607c005a10883e98dbdb540
Aldo <aldoyedro181@gmail.com>
2022-05-03 20:19:13 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
+const cors = require("cors"); // viene de node
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 require("./socket")(io);
-app.set("port", 3000);
+app.set("port", process.env.PORT || 5000);
 app.use(express.static(path.join(__dirname, "public"))); // Para tener acceso a los archivos public
+app.use(cors());
 
 server.listen(app.get("port"), () => {
   console.log("Running on " + app.get("port"));
